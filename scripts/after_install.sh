8af1f7f4459f44d961723b019c44606f2cc58055
hwan515 <stophwan515@gmail.com>
2024-01-23 16:50:28 +0900

    태초마을

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 LOGFILE=/home/ubuntu/gunicorn.log
 
 cd /home/ubuntu/ssg_backend || exit
@@ -16,5 +15,4 @@ chown -R ubuntu /home/ubuntu/ssg_backend
 sudo chown -R ubuntu:ubuntu /home/ubuntu/ssg_backend
 
 echo ">>> start server ---------------------"
-#gunicorn --bind 0.0.0.0:5000 --timeout 90 --log-level=debug "app:create_app()" >> "$LOGFILE" 2>&1 &
-flask run --host 0.0.0.0:5000 --timeout 90 --debug >> "$LOGFILE" 2>&1
+gunicorn --bind 0.0.0.0:5000 --timeout 90 "app:create_app()" >> "$LOGFILE" 2>&1 &
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# 이전에 실행된 gunicorn 또는 flask run 프로세스를 중지합니다.
-pkill -f 'flask run --host 0.0.0.0 --port 5000 --timeout 90 --debug'
+# 이전에 실행된 gunicorn 프로세스를 중지합니다.
+pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:create_app()'
 
 # 로그 파일과 프로젝트 디렉터리를 초기화합니다.
 rm -rf /home/ubuntu/gunicorn.log
 rm -rf /home/ubuntu/ssg_backend
 mkdir /home/ubuntu/ssg_backend
 
-cd /home/ubuntu/ssg_backend || exit
+cd /home/ubuntu/ssg_backend
