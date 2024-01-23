b0e6352c3e0dbad1cc686a7868600904e1ea51f3
윤규희 <ojinga0519@naver.com>
2024-01-23 15:37:49 +0900

    ss

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -15,4 +15,7 @@ chown -R ubuntu /home/ubuntu/ssg_backend
 sudo chown -R ubuntu:ubuntu /home/ubuntu/ssg_backend
 
 echo ">>> start server ---------------------"
-gunicorn --bind 0.0.0.0:5000 --timeout 90 --log-level=debug "app:create_app()" >> "$LOGFILE" 2>&1 &
+
+echo ">>> start server ---------------------"
+flask run --host 0.0.0.0 --port 5000 --timeout 90 --debug >> "$LOGFILE" 2>&1
+# gunicorn --bind 0.0.0.0:5000 --timeout 90 --log-level=debug "app:create_app()" >> "$LOGFILE" 2>&1 &
