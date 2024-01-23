63978c78adc192be6bff9fbe97deb8e2b97c2639
윤규희 <ojinga0519@naver.com>
2024-01-23 16:09:23 +0900

    ddd

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -17,4 +17,3 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/ssg_backend
 
 echo ">>> start server ---------------------"
 flask run --host 0.0.0.0 --port 5000 --timeout 90 --debug >> "$LOGFILE" 2>&1
-# gunicorn --bind 0.0.0.0:5000 --timeout 90 --log-level=debug "app:create_app()" >> "$LOGFILE" 2>&1 &
